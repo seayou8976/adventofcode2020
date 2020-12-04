1d5fd4f2d8c223a43bc944a5691e47c83d009cd3
seayou8976 <63001376+seayou8976@users.noreply.github.com>
2020-12-04 13:13:00 -0500

    Added Try/Catch/Throw logic

--- Day1/Get-Day1Answer.ps1
+++ Day1/Get-Day1Answer.ps1
@@ -7,27 +7,35 @@ Set-Location $PSScriptRoot
 $input = Get-Content "day1input.txt"
 
 Write-Host "++++++ Part 1 ++++++" -ForegroundColor Yellow
-:part1 foreach ($x in $input) {
-    foreach ($y in $input) {
-        if (([int]$x + [int]$y) -eq 2020) {
-            Write-Output "$x + $y = 2020"
-            $prod = ([int]$x * [int]$y)
-            Write-Output "Product is $prod"
-            Break part1
-        }
-    }
-}
-
-Write-Host "++++++ Part 2 ++++++" -ForegroundColor Yellow
-:part2 foreach ($x in $input) {
-    foreach ($y in $input) {
-        foreach ($z in $input) {
-            if (([int]$x + [int]$y + [int]$z) -eq 2020) {
-                Write-Output "$x + $y + $z = 2020"
-                $prod = ([int]$x * [int]$y * [int]$z)
+Try {
+    :part1 foreach ($x in $input) {
+        foreach ($y in $input) {
+            if (([int]$x + [int]$y) -eq 2020) {
+                Write-Output "$x + $y = 2020"
+                $prod = ([int]$x * [int]$y)
                 Write-Output "Product is $prod"
-                Exit
+                Break part1
             }
         }
     }
+} Catch {
+    Throw $_.Exception.Message
+}
+
+Write-Host "++++++ Part 2 ++++++" -ForegroundColor Yellow
+Try {
+    foreach ($x in $input) {
+        foreach ($y in $input) {
+            foreach ($z in $input) {
+                if (([int]$x + [int]$y + [int]$z) -eq 2020) {
+                    Write-Output "$x + $y + $z = 2020"
+                    $prod = ([int]$x * [int]$y * [int]$z)
+                    Write-Output "Product is $prod"
+                    Exit
+                }
+            }
+        }
+    }
+} Catch {
+    Throw $_.Exception.Message
 }
